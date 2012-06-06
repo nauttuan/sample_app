7612f1e7554bff3c5c256a70dea35a595b7cde29
Tuan <tanguyen1214@gmail.com>
2012-06-06 17:18:02 -0400

    Removed default Rails page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# Return a title on a per-page basis
+	def title
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if @title.nil?
+			return base_title
+		else
+			return "#{base_title} | #{@title}"
+		end
+	end
 end
+
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,9 @@
 SampleApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  ActiveSupport::Deprecation.silenced = true
+  
+
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
